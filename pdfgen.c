822dcda46ecacd9009cbc79015de6be78d1467f6
Andre Renaud <arenaud@designa-electronics.com>
2018-01-05 13:10:49 +1300

    dstr_ensure: Don't need to alloc if we're on the last character

--- pdfgen.c
+++ pdfgen.c
@@ -852,12 +852,11 @@ struct dstr {
 
 static int dstr_ensure(struct dstr *str, int len)
 {
-    if (str->alloc_len <= len) {
+    if (str->alloc_len < len) {
         int new_len = len + 4096;
         char *new_data = realloc(str->data, new_len);
-        if (!new_data) {
+        if (!new_data)
             return -ENOMEM;
-        }
         str->data = new_data;
         str->alloc_len = new_len;
     }
