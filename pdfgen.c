0a4972714daa4c2d85c577f900e30ea57f3c9ee9
Andre Renaud <arenaud@designa-electronics.com>
2018-04-09 08:42:51 +1200

    Prevent heap overflow when checking .jpg files
    Fixes #6

--- pdfgen.c
+++ pdfgen.c
@@ -2012,12 +2012,13 @@ static int jpeg_size(unsigned char* data, unsigned int data_size,
     if (i + 3 < data_size && data[i] == 0xFF && data[i+1] == 0xD8 &&
             data[i+2] == 0xFF && data[i+3] == 0xE0) {
         i += 4;
-        if(data[i+2] == 'J' && data[i+3] == 'F' && data[i+4] == 'I'
-                && data[i+5] == 'F' && data[i+6] == 0x00) {
+        if(i + 6 < data_size &&
+                data[i+2] == 'J' && data[i+3] == 'F' && data[i+4] == 'I' &&
+                data[i+5] == 'F' && data[i+6] == 0x00) {
             unsigned short block_length = data[i] * 256 + data[i+1];
             while(i<data_size) {
                 i+=block_length;
-                if(i >= data_size)
+                if((i + 1) >= data_size)
                     return -1;
                 if(data[i] != 0xFF)
                     return -1;
