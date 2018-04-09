3f4f5ca64eb510366dc4f4c84066847d974784f6
Andre Renaud <arenaud@designa-electronics.com>
2018-04-09 12:45:45 +1200

    Bookmarks: Fail early if the page is not available
    Prevents a bogus bookmark appearing in the system, which breaks future
    bookmarks

--- pdfgen.c
+++ pdfgen.c
@@ -834,13 +834,6 @@ int pdf_add_bookmark(struct pdf_doc *pdf, struct pdf_object *page,
                      int parent, const char *name)
 {
     struct pdf_object *obj;
-    if (!pdf_find_first_object(pdf, OBJ_outline))
-        if (!pdf_add_object(pdf, OBJ_outline))
-            return pdf->errval;
-
-    obj = pdf_add_object(pdf, OBJ_bookmark);
-    if (!obj)
-        return pdf->errval;
 
     if (!page)
         page = pdf_find_last_object(pdf, OBJ_page);
@@ -848,6 +841,14 @@ int pdf_add_bookmark(struct pdf_doc *pdf, struct pdf_object *page,
     if (!page)
         return pdf_set_err(pdf, -EINVAL,
                            "Unable to add bookmark, no pages available");
+
+    if (!pdf_find_first_object(pdf, OBJ_outline))
+        if (!pdf_add_object(pdf, OBJ_outline))
+            return pdf->errval;
+
+    obj = pdf_add_object(pdf, OBJ_bookmark);
+    if (!obj)
+        return pdf->errval;
 
     strncpy(obj->bookmark.name, name, sizeof(obj->bookmark.name));
     obj->bookmark.name[sizeof(obj->bookmark.name) - 1] = '\0';
