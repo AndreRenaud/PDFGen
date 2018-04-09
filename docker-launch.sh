c0b8fa9f5b17bbba756916c0cd369b50efb07edb
Andre Renaud <arenaud@designa-electronics.com>
2018-04-10 10:27:01 +1200

    docker: Need privileged mode to use fuzz sanitizer under Docker

--- docker-launch.sh
+++ docker-launch.sh
@@ -2,4 +2,4 @@
 # Utility script to build the docker development environment,
 # and then launch it with the current directory mounted inside.
 docker build -t pdfgen_image .
-docker run -ti -v `pwd`:`pwd` -w `pwd` -u `id -u`:`id -g` pdfgen_image
+docker run --privileged -ti -v `pwd`:`pwd` -w `pwd` -u `id -u`:`id -g` pdfgen_image
