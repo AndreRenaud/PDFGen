0a46e8ef435f03b77e202b9aa6adff6d11336046
Andre Renaud <andre@ignavus.net>
2019-03-15 11:24:36 +1300

    pdfgen: Fix possibly error string overrun
    Misunderstanding of how vsnprintf handles long messages

--- pdfgen.c
+++ pdfgen.c
@@ -431,6 +431,9 @@ static int pdf_set_err(struct pdf_doc *doc, int errval, const char *buffer,
     len = vsnprintf(doc->errstr, sizeof(doc->errstr) - 2, buffer, ap);
     va_end(ap);
 
+    if (len >= sizeof(doc->errstr) - 2)
+        len = sizeof(doc->errstr) - 2;
+
     /* Make sure we're properly terminated */
     if (doc->errstr[len] != '\n')
         doc->errstr[len] = '\n';
