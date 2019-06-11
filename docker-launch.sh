9514aa0b5e3a9e207eaff8209254a4b05aab1529
Andre Renaud <andre@ignavus.net>
2019-06-11 18:44:09 +1200

    Docker: For local launching, don't bother with the directory context

--- docker-launch.sh
+++ docker-launch.sh
@@ -2,5 +2,5 @@
 # Utility script to build the docker development environment,
 # and then launch it with the current directory mounted inside.
 set -e
-docker build -t pdfgen_image .
+docker build -t pdfgen_image - < Dockerfile
 docker run --privileged -ti -v `pwd`:`pwd` -w `pwd` -u `id -u`:`id -g` pdfgen_image
