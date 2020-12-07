6aff9ee35b128b9b7acdd8fc60183c7c8a888edb
Andre Renaud <andre@ignavus.net>
2020-12-08 09:05:54 +1300

    Added new fuzz tests

---
+++ tests/fuzz-bmp.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <string.h>
+
+#include "pdfgen.h"
+
+#define filename "./fuzz.bmp"
+int LLVMFuzzerTestOneInput(char *data, int size)
+{
+    FILE *temfile = fopen(filename, "w");
+    fwrite(data, 1, size, temfile);
+    fclose(temfile);
+
+    struct pdf_doc *pdf = pdf_create(PDF_A4_WIDTH, PDF_A4_HEIGHT, NULL);
+    pdf_set_font(pdf, "Times-Roman");
+    pdf_append_page(pdf);
+    pdf_add_bmp(pdf, NULL, 100, 500, 50, 150, filename);
+    pdf_save(pdf, "fuzz.pdf");
+    pdf_destroy(pdf);
+    return 0;
+}
