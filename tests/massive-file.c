43cee3c3381f1ac757c69f140974d01ee1898876
Andre Renaud <andre@ignavus.net>
2026-03-02 10:12:22 +1300

    typo

--- tests/massive-file.c
+++ tests/massive-file.c
@@ -11,21 +11,22 @@ int main(int argc, char **argv)
     if (argc > 1) {
         pagecount = atoi(argv[1]);
         if (pagecount < 1 || pagecount > INT_MAX) {
-            pagecount = 10;
+            fprintf(stderr, "Invalid page count: %d\n", pagecount);
+            return 1;
         }
-        pdf_set_font(pdf, "Times-Roman");
-        for (int i = 0; i < pagecount; i++) {
-            char str[64];
-            pdf_append_page(pdf);
-            sprintf(str, "page %d", i);
-            pdf_add_text(pdf, NULL, str, 12, 50, 20, PDF_BLACK);
-
-            pdf_add_image_file(pdf, NULL, 100, 500, 50, 150,
-                               "data/penguin.jpg");
-        }
-
-        sprintf(filename, "massive-%d.pdf", pagecount);
-        pdf_save(pdf, filename);
-        pdf_destroy(pdf);
-        return 0;
     }
+    pdf_set_font(pdf, "Times-Roman");
+    for (int i = 0; i < pagecount; i++) {
+        char str[64];
+        pdf_append_page(pdf);
+        sprintf(str, "page %d", i);
+        pdf_add_text(pdf, NULL, str, 12, 50, 20, PDF_BLACK);
+
+        pdf_add_image_file(pdf, NULL, 100, 500, 50, 150, "data/penguin.jpg");
+    }
+
+    sprintf(filename, "massive-%d.pdf", pagecount);
+    pdf_save(pdf, filename);
+    pdf_destroy(pdf);
+    return 0;
+}
